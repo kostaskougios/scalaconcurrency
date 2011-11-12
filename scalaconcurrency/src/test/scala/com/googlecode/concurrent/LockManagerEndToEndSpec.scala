62f6281f425a406fab631836c9985cf98e09027d
kostas.kougios <kostas.kougios@gmail.com>
2011-11-12 10:10:42 +0000

    lockManager end to end spec

--- scalaconcurrency/src/test/scala/com/googlecode/concurrent/LockManagerEndToEndSpec.scala
+++ scalaconcurrency/src/test/scala/com/googlecode/concurrent/LockManagerEndToEndSpec.scala
@@ -3,6 +3,9 @@ import org.specs2.mutable.SpecificationWithJUnit
 import java.util.concurrent.Executors
 import scala.concurrent.ops._
 import java.util.concurrent.atomic.AtomicInteger
+import scala.concurrent.ThreadPoolRunner
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 /**
  * @author kostantinos.kougios
  *
@@ -10,15 +13,45 @@ import java.util.concurrent.atomic.AtomicInteger
  */
 class LockManagerEndToEndSpec extends SpecificationWithJUnit {
 	"tryLockAndDo" in {
+		val pool = new ThreadPoolRunner {
+			val executor = Executors.newFixedThreadPool(10)
+			override def shutdown = {
+				executor.shutdown()
+				executor.awaitTermination(1000, TimeUnit.SECONDS)
+			}
+		}
 		val lock = LockManager.readWriteLock
 		val readers = new AtomicInteger
 		val writers = new AtomicInteger
-		val rfs = for (i <- 0 to 4)
-			yield future {
-			for (i <- 0 to 1000) {
-				Thread.sleep(1)
+		val errors = new AtomicInteger
+		val rfs = for (i <- 0 to 4) yield {
+			import pool._
+			execute { () =>
+				for (i <- 0 to 1000) {
+					lock.readLockAndDo {
+						if (writers.get > 0) errors.incrementAndGet
+						readers.incrementAndGet
+						Thread.sleep(1)
+						if (writers.get > 0) errors.incrementAndGet
+						readers.decrementAndGet
+					}
+					Thread.sleep(1)
+				}
+				for (i <- 0 to 1000) {
+					lock.writeLockAndDo {
+						if (readers.get > 0) errors.incrementAndGet
+						writers.incrementAndGet
+						Thread.sleep(1)
+						if (readers.get > 0) errors.incrementAndGet
+						writers.decrementAndGet
+					}
+					Thread.sleep(1)
+				}
 			}
 		}
-		true must beTrue
+		pool.shutdown()
+		readers.get must_== 0
+		writers.get must_== 0
+		errors.get must_== 0
 	}
 }
