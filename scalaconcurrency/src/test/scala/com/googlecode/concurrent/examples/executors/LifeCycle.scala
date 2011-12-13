218d997fe658c3f580f00afd9fee1b14141a7e2a
kostas.kougios <kostas.kougios@gmail.com>
2011-12-13 22:29:38 +0000

    more examples

--- scalaconcurrency/src/test/scala/com/googlecode/concurrent/examples/executors/LifeCycle.scala
+++ scalaconcurrency/src/test/scala/com/googlecode/concurrent/examples/executors/LifeCycle.scala
@@ -8,11 +8,25 @@ import com.googlecode.concurrent.ExecutorServiceManager
  */
 object LifeCycle extends App {
 
+	// ========================================================================================
+	// example 1
+	// ========================================================================================
 	// An executor will be created. The executor will have a pool of 5 threads.
 	// for every list item, it will execute the function using the executor (separate thread).
 	// It will collect the results and then shutdown the executor.
 	val results = ExecutorServiceManager.lifecycle(5, List(5, 10, 15, 20, 25, 30, 35, 40)) { param =>
-		100 + param
+		100 + param // this should be a relative heavy calculation
 	}
 	println(results) // List(105, 110, 115, 120, 125, 130, 135, 140)
+
+	// ========================================================================================
+	// example 2
+	// ========================================================================================
+	// create an executor of 5 threads and submit the function 20 times.
+	// each time the function is called, i is between 1..20. The function
+	// results are appended to a collection
+	val results2 = ExecutorServiceManager.lifecycle(5, 20) { i =>
+		100 + i
+	}
+	println(results2) // Vector(101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120)
 }
